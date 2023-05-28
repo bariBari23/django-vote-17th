9e8ca75f60b69400dc38e175939426b6e07b3c02
yumiiiiiii <102947194+yumiiiiiii@users.noreply.github.com>
2023-05-28 22:38:33 +0900

    feat:내 팀 투표 불가

--- votes/views.py
+++ votes/views.py
@@ -15,11 +15,6 @@ class TeamVoteView(APIView):
         vote = Team_Vote.objects.all()
         serializer = TeamVoteSerializer(vote, many=True)
         vote_count = Team_Vote.objects.filter().values('team').annotate(total=Count('team')).order_by('-total')
-        # baribari = Team_Vote.objects.filter(team='바리바리').count()
-        # repick = Team_Vote.objects.filter(team='RePick').count()
-        # hooking = Team_Vote.objects.filter(team='Hooking').count()
-        # dansupport = Team_Vote.objects.filter(team='Dansupport').count()
-        # therapese = Team_Vote.objects.filter(team='TherapEse').count()
         return Response({'message': "투표 조회", 'vote_count':vote_count, 'data': serializer.data}, status=HTTP_200_OK)
 
     def post(self, request, format=None):
@@ -31,9 +26,12 @@ class TeamVoteView(APIView):
                 delete_vote.delete()
                 return Response({'message': '투표 취소', 'data': serializer.data}, status=HTTP_201_CREATED)
             else:
-                serializer.save(user=self.request.user, team=serializer.validated_data['team'])
-                return Response({'message':'투표 성공', 'data': serializer.data}, status=HTTP_201_CREATED)
-        return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
+                if user.team==serializer.validated_data['team']:
+                    return Response({'message': '내 팀은 투표 할 수 없습니다!', 'data': serializer.errors}, status=HTTP_400_BAD_REQUEST)
+                else:
+                    serializer.save(user=self.request.user, team=serializer.validated_data['team'])
+                    return Response({'message':'투표 성공', 'data': serializer.data}, status=HTTP_201_CREATED)
+        return Response({'message': '투표 실패', 'data': serializer.errors}, status=HTTP_400_BAD_REQUEST)
 
 
 class PartVoteView(APIView):
