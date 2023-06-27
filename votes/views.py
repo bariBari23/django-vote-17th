216408cbdec8233d0056f68edf6c0f6214c7be40
yumiiiiiii <102947194+yumiiiiiii@users.noreply.github.com>
2023-06-27 20:38:44 +0900

    mod : user data

--- votes/views.py
+++ votes/views.py
@@ -16,7 +16,10 @@ class TeamVoteView(APIView):
         vote = Team_Vote.objects.all()
         serializer = TeamVoteSerializer(vote, many=True)
         vote_count = Team_Vote.objects.filter().values('team').annotate(total=Count('team')).order_by('-total')
-        return Response({'message': "투표 조회", 'vote_count':vote_count, 'data': serializer.data}, status=HTTP_200_OK)
+        user = request.user
+        user_data = get_object_or_404(User, pk=user.id)
+        serializer_user = UserLoginSerializer(user_data)
+        return Response({'message': "투표 조회", 'vote_count':vote_count, 'data': serializer.data, 'user':serializer_user.data}, status=HTTP_200_OK)
 
     # def post(self, request, format=None):
     #     user = self.request.user
